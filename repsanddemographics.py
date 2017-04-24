1ec922c300b5f6654ef734abe05102cbe3345954
nicoledan <nicole_k_dan@hotmail.com>
2017-04-24 00:20:32 -0400

    second commit

--- repsanddemographics.py
+++ repsanddemographics.py
@@ -8,40 +8,44 @@ application = app
 
 #from repdata.py
 def get_districts_reps_and_parties (source):
-    districts_reps_and_parties =[]
+    districts_reps_and_parties=[]
     for row in source:
             district = row["district"]
             rep = row["name"]
             party = row["party"]
-            return districts_reps_and_parties
+            districts_reps_and_parties.append([district, rep, party])
+    return districts_reps_and_parties
 
 #from datasets.py
 
-def get_ids_and_districts (source):
-    ids_and_districts = []
-    for row in source:
-        id = row["id"]
-        district = row["district"]
-        ids_and_districts.append([id, district])
-    return ids_and_districts
+#def get_ids_and_districts (source):
+    #ids_and_districts = []
+    #for row in source:
+        #id = row["id"]
+        #district = row["district"]
+        #ids_and_districts.append([id, district])
+    #return ids_and_districts
 
 
 @app.route('/')
 @app.route('/index/')
 def index():
-    ids_and_districts = get_ids_and_districts (DEMOGRAPHICS)
-    return render_template('index.html', pairs=ids_and_districts)
+    districts_reps_and_parties = get_districts_reps_and_parties(REPS)
+    return render_template('index.html', pairs=districts_reps_and_parties)
 
 
-@app.route('/detail/<district>)
+@app.route('/detail/<district>')
 
 def detail(district):
     DISTRICTS[district]
-    eighteen_twentynine = DISTRICTS[district][1]
-    thirty_fortyfour = DISTRICTS[district][2]
-    fortyfive_sixtyfour = DISTRICTS[district][3]
-    sixtyfive_over = DISTRICTS[district][4]
-    return render_template ('detail.html', eighteen_twentynine = eighteen_twentynine, thirty_fortyfour = thirty_fortyfour, fortyfive_sixtyfour = fortyfive_sixtyfour, sixtyfive_over = sixtyfive_over)
+    district = district
+    total_citizens = DISTRICTS[district][1]
+    eighteen_twentynine = DISTRICTS[district][2]
+    thirty_fortyfour = DISTRICTS[district][3]
+    fortyfive_sixtyfour = DISTRICTS[district][4]
+    sixtyfive_over = DISTRICTS[district][5]
+    districts_reps_and_parties = get_districts_reps_and_parties(REPS)
+    return render_template ('detail.html', district = district, eighteen_twentynine = eighteen_twentynine, thirty_fortyfour = thirty_fortyfour, fortyfive_sixtyfour = fortyfive_sixtyfour, sixtyfive_over = sixtyfive_over)
     #id, district, total_citizens, eighteen_twentynine, thirty_fortyfour, fortyfive_sixtyfour, sixtyfive_over = get_demographicdata(DEMOGRAPHICS,id)
     #return render_template('detail.html', district = district, total_citizens = total_citizens, eighteen_twentynine = eighteen_twentynine, thirty_fortyfour = thirty_fortyfour, fortyfive_sixtyfour = fortyfive_sixtyfour, sixtyfive_over = sixtyfive_over )
 
